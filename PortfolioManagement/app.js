9ea7ee51d99d167d071e2101ebc10c0945f6d831
Aj163 <a3sajs@gmail.com>
2018-10-25 13:54:58 +0530

    Login CSS

--- PortfolioManagement/app.js
+++ PortfolioManagement/app.js
@@ -27,6 +27,7 @@ var bodyParser = require('body-parser');
 var userIDs = {};
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
